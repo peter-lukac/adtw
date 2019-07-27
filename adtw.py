d917e196a7b3f22f2ce31c68e8cd4e3e5342580d
peter <peterlukac96@gmail.com>
2019-07-27 22:00:57 +0200

    new version of backtracking implemented

--- adtw.py
+++ adtw.py
@@ -3,6 +3,20 @@ from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
 
+PATH_1 = 1
+PATH_3_1 = sqrt(10)
+PATH_2_1 = sqrt(5)
+PATH_1_1 = sqrt(2)
+
+UPWARD_1 = 1
+UPWARD_3_1 = 2
+UPWARD_2_1 = 3
+DIAGONAL = 4
+RIGHT_2_1 = 5
+RIGHT_3_1 = 6
+RIGHT_1 = 7
+
+
 def get_area(width, height, start_padding, middle_padding):
     horizontal_d = height/width
     vertical_d = width/height
@@ -40,6 +54,75 @@ def get_area(width, height, start_padding, middle_padding):
     return upper, lower
 
 
+def get_backtraced_path(local_distance_matrix):
+    x = local_distance_matrix.shape[0]
+    y = local_distance_matrix.shape[1]
+
+    cumulated_distance_matrix = zeros((x,y))
+    cumulated_distance_matrix[:] = inf
+    cumulated_distance_matrix[0, 0] = local_distance_matrix[0, 0]
+
+    backtrace_matrix = zeros((x,y))
+
+    open_cells = []
+    open_cells.insert(0, [0,0])
+
+    while open_cells:
+        o = open_cells.pop()
+
+        if o[0] >= x or o[1] >= y:
+            continue
+
+        #path 4
+        if o[0] < x-1 and o[1] < y-1:
+            d = cumulated_distance_matrix[o[0], o[1]] + (local_distance_matrix[o[0]+1, o[1]+1] * PATH_1_1)
+            if d < cumulated_distance_matrix[o[0]+1, o[1]+1]:
+                cumulated_distance_matrix[o[0]+1, o[1]+1] = d
+                backtrace_matrix[o[0]+1, o[1]+1] = DIAGONAL
+                if not [o[0]+1, o[1]+1] in open_cells:
+                    open_cells.insert(0, [o[0]+1, o[1]+1])
+
+        #path 1
+        if o[0] < x-1:
+            d = cumulated_distance_matrix[o[0], o[1]] + local_distance_matrix[o[0]+1, o[1]]
+            if d < cumulated_distance_matrix[o[0]+1, o[1]]:
+                cumulated_distance_matrix[o[0]+1, o[1]] = d
+                backtrace_matrix[o[0]+1, o[1]] = UPWARD_1
+                if not [o[0]+1, o[1]] in open_cells:
+                    open_cells.insert(0, [o[0]+1, o[1]])
+
+        #path 7
+        if o[1] < y-1:
+            d = cumulated_distance_matrix[o[0], o[1]] + local_distance_matrix[o[0], o[1]+1]
+            if d < cumulated_distance_matrix[o[0], o[1]+1]:
+                cumulated_distance_matrix[o[0], o[1]+1] = d
+                backtrace_matrix[o[0], o[1]+1] = RIGHT_1
+                if not [o[0], o[1]+1] in open_cells:
+                    open_cells.insert(0, [o[0], o[1]+1])
+
+    path = [[x-1, y-1]]
+    while not [0, 0] in path:
+        #path 4
+        if backtrace_matrix[path[0][0], path[0][1]] == DIAGONAL:
+            path.insert(0, [path[0][0]-1, path[0][1]-1])
+        #path 1
+        if backtrace_matrix[path[0][0], path[0][1]] == UPWARD_1:
+            path.insert(0, [path[0][0]-1, path[0][1]])
+        #path 7
+        if backtrace_matrix[path[0][0], path[0][1]] == RIGHT_1:
+            path.insert(0, [path[0][0], path[0][1]-1])
+
+    plt.subplot(1,3,1)
+    plt.pcolormesh(local_distance_matrix)
+    plt.subplot(1,3,2)
+    plt.pcolormesh(cumulated_distance_matrix)
+    plt.subplot(1,3,3)
+    plt.pcolormesh(backtrace_matrix)
+    plt.show()
+
+    return path
+
+
 def adtw(des_array, fit_array, start_padding=5, middle_padding=0.50):
     height = des_array.shape[1]
     width = fit_array.shape[1]
@@ -54,5 +137,8 @@ def adtw(des_array, fit_array, start_padding=5, middle_padding=0.50):
     for i in range(0, width):
         for j in range(lower[i], upper[i]):
             distance_matrix[j,i] = sqrt(sum(power((des_array.T[j]-fit_array.T[i]), 2)))
+
+
+    get_backtraced_path(distance_matrix)
 
     return distance_matrix
